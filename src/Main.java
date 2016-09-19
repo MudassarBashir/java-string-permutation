d774775ed97f52a784ecbb7eaf59e16e92db8e1d
Mudassar Bashir <moeb59@gmail.com>
2016-09-18 20:17:59 -0400

    Further trying to improve spatial footprint by using StringBuilder.

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,24 @@ public class Main {
 
     private static void permutateString(String str) {
 
-        List<String> results = permutations(str, new ArrayList<String>());
+        List<String> results = permutations(new StringBuilder(str), new ArrayList<>());
         System.out.println("Number of permutations: " + results.size()
                             + "\n" + "Permutations: \n" + results);
     }
 
-    private static List<String> permutations(String input, List<String> permutationsList) {
+    private static List<String> permutations(StringBuilder input, List<String> permutationsList) {
 
-        if (input.equals("") || input.length() == 1) {
-            permutationsList.add(input);
+        if (input.length() == 0 || input.length() == 1) {
+            permutationsList.add(input.toString());
         }
         else {
             char prefix;
-            StringBuffer substring;
+            StringBuilder substring;
 
             for (int i=0; i<input.length(); i++) {
                 prefix = input.charAt(i);
-                substring = new StringBuffer(input).deleteCharAt(i);
-                for (String str : permutations(substring.toString(), new ArrayList<String>())) {
+                substring = new StringBuilder(input).deleteCharAt(i);
+                for (String str : permutations(substring, new ArrayList<>())) {
                     permutationsList.add(String.valueOf(prefix) + str);
                 }
             }
